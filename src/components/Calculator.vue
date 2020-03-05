e449b77e0f944da2b59b82fb4594f0b44de5ff88
ohebron <admin@admins-MacBook-Pro.local>
2020-03-04 21:41:10 -0800

    added btns and operators

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,18 +1,59 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-
+  <div class="calculator-grid">
+    <div class="mid-grid">1111</div>
+    <div class="btn">%</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">÷</div>
+    <div class= "btn">7</div>
+    <div class= "btn">8</div>
+    <div class= "btn">9</div>
+    <div class= "btn">x</div>
+    <div class= "btn">4</div>
+    <div class= "btn">5</div>
+    <div class= "btn">6</div>
+    <div class= "btn">-</div>
+    <div class= "btn">1</div>
+    <div class= "btn">2</div>
+    <div class= "btn">3</div>
+    <div class= "btn">+</div>
+    <div class=" btn zero">0</div>
+    <div class= "btn">.</div>
+    <div class= "btn">=</div>
   </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
 
 
 <style scoped>
+.calculator-grid{
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: minmax(30px, auto);
+  background-color: rgb(255, 255, 255);
+  
+  
+}
+.mid-grid{
+  background-color: rgb(185, 174, 174);
+  grid-column: 1 /5;
+  border: solid 1px black;
 
+}
+.btn {
+  border: solid 1px black
+}
+.zero{
+  grid-column: 1/3;
+
+  
+}
 </style>
