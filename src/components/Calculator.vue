7439215ab051067c916c6664019517c0a00cf7a7
ohebron <admin@admins-MacBook-Pro.local>
2020-03-05 18:25:57 -0800

    added method clear; to clear any displayed number in the main cal bar

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="calculator-grid">
-    <div class="mid-grid">1111</div>
-    <div class="btn">%</div>
-    <div class="btn">C</div>
+    <div class="mid-grid">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
     <div class="btn">+/-</div>
-    <div class="btn">÷</div>
+    <div class="btn">%</div>
+    <div class="btn operator">÷</div>
     <div class= "btn">7</div>
     <div class= "btn">8</div>
     <div class= "btn">9</div>
-    <div class= "btn">x</div>
+    <div class= "btn operator">x</div>
     <div class= "btn">4</div>
     <div class= "btn">5</div>
     <div class= "btn">6</div>
-    <div class= "btn">-</div>
+    <div class= "btn operator">-</div>
     <div class= "btn">1</div>
     <div class= "btn">2</div>
     <div class= "btn">3</div>
-    <div class= "btn">+</div>
+    <div class= "btn operator">+</div>
     <div class=" btn zero">0</div>
     <div class= "btn">.</div>
-    <div class= "btn">=</div>
+    <div class= "btn operator">=</div>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+  return {
+    current: '',
+   }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    }
+  }
 }
+
 </script>
 
 
@@ -33,27 +44,28 @@ export default {
 <style scoped>
 .calculator-grid{
   margin: 0 auto;
-  width: 400px;
-  font-size: 40px;
+  width: 350px;
+  font-size: 45px;
   display: grid;
   grid-template-columns: repeat(4,1fr);
   grid-auto-rows: minmax(30px, auto);
   background-color: rgb(255, 255, 255);
-  
-  
 }
+
 .mid-grid{
-  background-color: rgb(185, 174, 174);
+  background-color: rgb(226, 218, 218);
   grid-column: 1 /5;
-  border: solid 1px black;
+  border: solid 1px rgb(14, 13, 13);
 
 }
 .btn {
-  border: solid 1px black
+  border: solid 1px rgb(194, 189, 189)
 }
 .zero{
   grid-column: 1/3;
-
-  
+}
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
